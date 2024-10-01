15ad4aafde4e7d4352a3279d4be061b538741f0c
SupertigerDev <saawalkhn@googlemail.com>
2024-10-01 11:55:55 +0100

    remove env.ts from .gitignore

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+export const env = {
+  attachmentMaxBodyLength: parseInt(process.env.ATTACHMENT_MAX_BODY_LENGTH!) * (1024 * 1024),
+  imageMaxBodyLength: parseInt(process.env.IMAGE_MAX_BODY_LENGTH!) * (1024 * 1024),
+  expireFileMS: (1000 * 60 * 60 * 24) * parseInt(process.env.EXPIRE_FILE_MS!),
+  secret: process.env.SECRET!,
+  devMode: process.env.DEV_MODE?.toLowerCase() === "true",
+  databaseUrl: process.env.DATABASE_URL!
+}
