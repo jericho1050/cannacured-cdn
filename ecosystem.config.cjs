e26a141cee9d0fb6473585076a5e21ea5298ef6e
SupertigerDev <saawalkhn@googlemail.com>
2024-10-05 12:56:56 +0100

    disable watch

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,8 +4,7 @@ module.exports = {
       name: 'nerimity-cdn',
       script: 'npm',
       args: 'run start',
-      watch: true,
-      ignore_watch: ['node_modules', 'dist', 'public', 'temp'],
+      watch: false,
       time: true
     },
   ],
