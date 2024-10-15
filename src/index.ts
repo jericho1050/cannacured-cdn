525dcb11740fb519d63116c4d58b32a4874984be
Supertiger <saawalkhn@googlemail.com>
2024-10-15 08:42:40 +0100

    log line number for each console log

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,35 @@ import { env } from "./env";
 import { removeExpiredFiles } from "./ExpireFileService";
 const cpus = env.devMode ? 1 : os.cpus().length;
 
+
+['log', 'warn', 'error'].forEach((methodName) => {
+  const originalMethod = console[methodName];
+  console[methodName] = (...args) => {
+    let initiator = 'unknown place';
+    try {
+      throw new Error();
+    } catch (e) {
+      if (typeof e.stack === 'string') {
+        let isFirst = true;
+        for (const line of e.stack.split('\n')) {
+          const matches = line.match(/^\s+at\s+(.*)/);
+          if (matches) {
+            if (!isFirst) { // first line - current function
+              // second line - caller (what we are looking for)
+              initiator = matches[1];
+              break;
+            }
+            isFirst = false;
+          }
+        }
+      }
+    }
+    originalMethod.apply(console, [...args, '\n', `  at ${initiator}`]);
+  };
+});
+
+
+
 if (cluster.isPrimary) {
 
   createFolders();
